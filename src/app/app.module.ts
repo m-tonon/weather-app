3f9830c59a615446c57fa5ab5e7977d3dc9dae4f
m-tonon <math.tonon@gmail.com>
2023-01-25 07:35:48 -0300

    created a service to get weather data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LowerDataComponent } from './lower-data/lower-data.component';
 import { UpperDataComponent } from './upper-data/upper-data.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { UpperDataComponent } from './upper-data/upper-data.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
