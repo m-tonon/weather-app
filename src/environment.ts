87f81c68f320a64e59072ba68e582db6fb53ebb3
m-tonon <math.tonon@gmail.com>
2023-01-25 07:34:21 -0300

    setting api info

---
+++ src/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  weatherApiBaseUrl: "https://open-weather13.p.rapidapi.com",
+  XRapidAPIKeyHeaderName: "X-RapidAPI-Key",
+  XRapidAPIKeyHeaderValue: "0a104ba9a3msheb88692cc25c1dbp129f44jsne09bac3f50d5",
+  XRapidAPIHostHeaderName: "X-RapidAPI-Host",
+  XRapidAPIHostHeaderValue: "open-weather13.p.rapidapi.com"
+}
